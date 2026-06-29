ee1a0552c97ac45a64462d4aa678887b48daab6a
Stanley Nguyen <stan.ng66@gmail.com>
2026-06-29 17:36:10 -0400

    Create a header.js to implement a header with a title and cart item counter

---
+++ src/header.js
@@ -0,0 +1,18 @@
+// ====================
+// Assignment 1
+// Stanley Nguyen
+// Humber College
+// CPAN 
+// Ronak 
+// July 10, 2026
+// --------------------
+// header.js displays the tile and displays cart item count
+// ====================
+export default function Header({cartCount}) {
+    return (
+        <header style={{}}>   {/* Add css styling here */}
+            <h1> Food Menu App </h1>                 {/* Title of the app */}
+            <p> Cart: {cartItemCounter} items </p>   {/* Shows number of items in the cart */}
+        </header>
+    )
+}
