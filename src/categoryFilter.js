048c1a53fcb147fce7bd745ddaaab0d32fdae451
Stanley Nguyen <stan.ng66@gmail.com>
2026-06-30 10:47:31 -0400

    Built a general function to filter category. Add a proposal paragraph for stage 1. Add a txt file for any resources I found useful in helping in creating my application

---
+++ src/categoryFilter.js
@@ -0,0 +1,27 @@
+// ====================
+// Assignment 1
+// Stanley Nguyen
+// Humber College
+// CPAN 
+// Ronak 
+// July 10, 2026
+// --------------------
+// categoryFilter.js lets the user choose a category through a filter button
+// ====================
+
+export default function CategoryFilter({ categories, selectedCategory, setSelectedCategory}) {   // function({}) uses {} to recieve props for react
+    return (
+        <div style={{margin: "20px"}}>
+            {categories.map((categoryType) => (
+                <button key={categoryType}
+                   onClick={() => setSelectedCategory(categoryType)}
+                   style={{   // css styling the button, could move this to its own css file.
+
+                   }}
+                >
+                    {categoryType}
+                </button>   
+            ))}
+        </div>
+    );
+}
