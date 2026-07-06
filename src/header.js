dc3f1942d50da072c7f74d3c65c67a88968b8fee
Stanley Nguyen <stan.ng66@gmail.com>
2026-07-06 11:03:24 -0400

    Add a cart emoticon for my cart counter

--- src/header.js
+++ src/header.js
@@ -10,11 +10,18 @@
 // ====================
 export default function Header({cartCount}) {
     return (
-        <header className="app-header">   
+        <header className="app-header">
+            <div className="header-title">
             <h1> Food Menu App </h1>   {/* Title of the app */}
             <h2> Stanley Nguyen</h2>
             <h3> Assignment 1 </h3>
-            <p> Cart: {cartCount} items </p>   {/* Shows number of items in the cart */}
+            </div> 
+
+            <div className="header-cart">
+            {/*<p> Cart: {cartCount} items </p> */}  {/* Shows number of items in the cart */}
+            <p><i className="fa-solid fa-cart-shopping"></i> {cartCount} items</p>
+            </div>
+
         </header>
     )
 }
